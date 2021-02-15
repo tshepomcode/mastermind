e4ae2fce6997ea2070b64cd202c4a4d014f8b33f
tshepomcode <tshepomcode@gmail.com>
2021-02-15 16:27:07 +0200

    pseudo code added

---
+++ mastermind.rb
@@ -0,0 +1,42 @@
+## Players to choose where codemaker or codebreaker
+
+# Choose (1) codemaker or (2) comebreaker
+# Choose number of games to play (best of 3?)
+# Choose number of rows (8 - 12)
+
+#### To make the game challenging, should we allow:
+#### -  repeat colours for code maker or only unique colours?
+#### -  response (review) from codemaker in order or out of order
+####    selection? That is, position 1 review 1 colour 1
+#### 
+
+# Begin game
+
+# (Game 1 of 3)
+## Setup board of (8?) rows
+
+# -> codemaker 
+#   -   codemaker must place 4 colour code pegs to be broken/guessed
+# (Row 1 of 8?)
+# -> code breaker to enter first row of guesses (4 colour pegs)
+#   -   after guess, codemaker must place(review?) 
+#       - white peg if : correct colour but wrong position
+#       - black peg if : correct colour and correct position
+#       - no pegs for no matches
+#   -   if (all 4 placements are correct)
+#           codebreaker wins
+#           break (next game 2 of 3)
+#       else
+#           Provide feedback (white, black or no pegs)
+#           play continues
+# (Row 2 of 8?)
+#   -   Display feedback from codemaker (white, black or no peg)
+# -> code breaker to enter second row of guesses (4 colour pegs)
+#    -   codemaker must place(review?) (white, black, no peg)
+#       - if (correct [slot1, slot2, slot3, slot4])
+#           codebreaker wins
+#           break (next game 3 of 3)
+#        
+
+
+## Setup board of 8 rows
